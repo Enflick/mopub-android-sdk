c7b4b10bf213fd98557d30d847e3c24058417466
Armando Murga <armando.murga@enflick.com>
2018-08-08 02:29:37 -0400

    .

--- mopub-sdk/mopub-sdk-base/src/main/java/com/mopub/common/AdReport.java
+++ mopub-sdk/mopub-sdk-base/src/main/java/com/mopub/common/AdReport.java
@@ -12,6 +12,7 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
+ *
  * A value class used for generating reports to send data back to MoPub
  */
 public class AdReport implements Serializable {
